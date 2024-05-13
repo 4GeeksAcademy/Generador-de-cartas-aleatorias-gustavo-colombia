e8ce457dfdb2e095b3bd30bcf07e2d87c1f6153e
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-05-13 23:59:02 +0000

    message

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♥", "♣", "♦", "♠"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const upCard = document.getElementById("card-icon-top");
+  const centerCard = document.getElementById("card-number");
+  const downCard = document.getElementById("card-icon-bottom");
+
+  function randomIcon(icons) {
+    return icons[Math.floor(Math.random() * icons.length)];
+  }
+
+  function randomNumber(numbers) {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+  }
+
+  const icon = randomIcon(suits);
+  const cartaNum = randomNumber(numbers);
+
+  upCard.innerHTML = icon;
+  downCard.innerHTML = icon;
+  centerCard.innerHTML = cartaNum;
 };
